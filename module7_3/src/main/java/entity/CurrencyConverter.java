6e8c3a9feba3590dcb25e6a8491225d54c726430
Samu <samupie@metropolia.fi>
2024-05-11 11:36:22 +0300

    7.3  somehow done

---
+++ module7_3/src/main/java/entity/CurrencyConverter.java
@@ -0,0 +1,50 @@
+package entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "currencies")
+
+public class CurrencyConverter {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String code;
+    private String symbol;
+    private double rate;
+
+    public CurrencyConverter() {
+    }
+    public CurrencyConverter(String name, String code, String symbol, double rate) {
+        this.name = name;
+        this.code = code;
+        this.symbol = symbol;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public double getRate() {
+        return rate;
+    }
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+}
